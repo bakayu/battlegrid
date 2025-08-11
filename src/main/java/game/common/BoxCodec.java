642ac26c15b9557726e41251dc66787f3ae1667c
Anmol Puri <anmolpuri954@gmail.com>
2025-08-12 01:18:20 +0530

    feat(encryption): add RSA encryption (#1)
    * feat(encryption): add RSA encryption
    
    * test: add test for Box class
    
    Signed-off-by: Ayush <mail@ayuch.dev>
    
    * resolve merge conflict
    
    Signed-off-by: Anmol Puri <anmolpuri954@gmail.com>
    
    * fix pom.xml
    
    Signed-off-by: Anmol Puri <anmolpuri954@gmail.com>
    
    * feat(encryption): Migrate to standard JCA
    
    * fix pom.xml
    
    Signed-off-by: Anmol Puri <anmolpuri954@gmail.com>
    
    ---------
    
    Signed-off-by: Ayush <mail@ayuch.dev>
    Signed-off-by: Anmol Puri <anmolpuri954@gmail.com>
    Co-authored-by: Ayush <mail@ayuch.dev>
    Co-authored-by: Ayush Chauhan <56398566+bakayu@users.noreply.github.com>

--- src/main/java/game/common/BoxCodec.java
+++ src/main/java/game/common/BoxCodec.java
@@ -6,22 +6,44 @@ import jakarta.websocket.EncodeException;
 import jakarta.websocket.Encoder;
 import jakarta.websocket.EndpointConfig;
 
+import java.security.PrivateKey;
+import java.security.PublicKey;
 /**
  * Handles both encoding (Object -> String) and decoding (String -> Object) for Box class.
  */
 public class BoxCodec implements Encoder.Text<Box>, Decoder.Text<Box> {
 
-	// Encoder
-	@Override
-	public String encode(Box box) throws EncodeException {
-		return box.toEncryptedString();
-	}
+    // Keys are stored as instance variables
+    private PublicKey publicKey;
+    private PrivateKey privateKey;
+
+    // Use a default constructor to avoid errors with Tyrus, and a setter
+    // to provide the keys after the object is created.
+    public BoxCodec() {}
+
+    public void setKeys(PublicKey publicKey, PrivateKey privateKey) {
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
+    }
+
+    @Override
+    public String encode(Box box) throws EncodeException {
+        // We use the public key to encrypt data for the other party.
+        if (this.publicKey == null) {
+            throw new EncodeException(box, "Public key not set for encoding");
+        }
+        return box.toEncryptedString(this.publicKey);
+    }
 
 	// Decoder
 	@Override
-	public Box decode(String s) throws DecodeException {
-		return Box.fromEncryptedString(s);
-	}
+    public Box decode(String s) throws DecodeException {
+        // We use our private key to decrypt data received from the other party.
+        if (this.privateKey == null) {
+            throw new DecodeException(s, "Private key not set for decoding");
+        }
+        return Box.fromEncryptedString(s, this.privateKey);
+    }
 
 	@Override
 	public boolean willDecode(String s) {
--- src/test/java/game/common/BoxTest.java
+++ src/test/java/game/common/BoxTest.java
@@ -3,18 +3,24 @@ package game.common;
 import com.google.gson.JsonObject;
 import org.junit.jupiter.api.Test;
 
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoxTest {
 
 	@Test
-	void testToEncryptedString_AND_FromEncryptedString() {
+	void testToEncryptedString_AND_FromEncryptedString() throws NoSuchAlgorithmException, NoSuchProviderException {
 		JsonObject payload = new JsonObject();
 		payload.addProperty("key", "value");
+
+        KeyPair keyPair = Box.generateKeyPair();
 		Box originalBox = new Box(payload);
 
-		String encryptedString = originalBox.toEncryptedString();
-		Box decryptedBox = Box.fromEncryptedString(encryptedString);
+		String encryptedString = originalBox.toEncryptedString(keyPair.getPublic());
+		Box decryptedBox = Box.fromEncryptedString(encryptedString, keyPair.getPrivate());
 
 		assertEquals(payload, decryptedBox.getPayload());
 	}
