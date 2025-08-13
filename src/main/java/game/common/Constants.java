e7e21b621ba0613d9272aa6454b263a429ecba37
Anmol Puri <anmolpuri954@gmail.com>
2025-08-13 16:02:32 +0530

    add `common.Constants` to store Constants
    Signed-off-by: Anmol Puri <anmolpuri954@gmail.com>

---
+++ src/main/java/game/common/Constants.java
@@ -0,0 +1,17 @@
+package game.common;
+
+/**
+ * This class acts as a shared space to store all Constants for the game
+ * battlegrid.
+ */
+public final class Constants {
+    private Constants() {
+        // This private constructor to prevent instantiation.
+    }
+
+    public static final String DEFAULT_USERNAME = "AnonymousPlayer";
+
+    public static final String PROPERTY_AES_KEY = "aesKey";
+    public static final String PROPERTY_USERNAME = "username";
+
+}
